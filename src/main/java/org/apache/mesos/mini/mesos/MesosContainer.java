7c1e6279e3c2fa621cf8b44012baff763c613a72
Phil Winder <pnw@trifork.com>
2015-07-20 15:26:16 +0100

    Depreciated containerId method (Missing file).

--- src/main/java/org/apache/mesos/mini/mesos/MesosContainer.java
+++ src/main/java/org/apache/mesos/mini/mesos/MesosContainer.java
@@ -41,6 +41,11 @@ public class MesosContainer extends AbstractContainer {
         return "mini_mesos_cluster_" + new SecureRandom().nextInt();
     }
 
+    /**
+     * @deprecated Please use the method getContainerId(), which is generic to all {@link AbstractContainer}'s
+     * @return Container ID.
+     */
+    @Deprecated
     public String getMesosContainerID() {
         return getContainerId();
     }
