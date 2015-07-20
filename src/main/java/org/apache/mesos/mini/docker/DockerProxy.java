c2c4aa477cb43d1933b3253bbb91a4bc3584aacc
Frank Scholten <frank@frankscholten.nl>
2015-07-20 15:09:31 +0200

    #13 tinyproxy container name now has a random suffix..

--- src/main/java/org/apache/mesos/mini/docker/DockerProxy.java
+++ src/main/java/org/apache/mesos/mini/docker/DockerProxy.java
@@ -6,6 +6,8 @@ import com.github.dockerjava.api.model.ExposedPort;
 import com.github.dockerjava.api.model.PortBinding;
 import org.apache.mesos.mini.container.AbstractContainer;
 
+import java.security.SecureRandom;
+
 public class DockerProxy extends AbstractContainer {
 
     public static final String PROXY_IMAGE = "paintedfox/tinyproxy";
@@ -24,6 +26,15 @@ public class DockerProxy extends AbstractContainer {
 
     @Override
     protected CreateContainerCmd dockerCommand() {
-        return dockerClient.createContainerCmd(PROXY_IMAGE).withName(CONTAINER_NAME).withExposedPorts(ExposedPort.parse("" + proxyPort)).withPortBindings(PortBinding.parse("0.0.0.0:" + proxyPort + ":8888"));
+        return dockerClient
+                .createContainerCmd(PROXY_IMAGE)
+                .withName(generateRegistryContainerName())
+                .withExposedPorts(ExposedPort.parse("" + proxyPort))
+                .withPortBindings(PortBinding.parse("0.0.0.0:" + proxyPort + ":8888"));
     }
+
+    String generateRegistryContainerName() {
+        return CONTAINER_NAME + "_" + new SecureRandom().nextInt();
+    }
+
 }
