73f05693437e92b3355c7dbd7de6d3e5368828b2
Martin Westergaard Lassen <mwl@trifork.com>
2015-08-04 10:55:53 +0100

    Removed redundant inner class

--- src/test/java/org/apache/mesos/mini/DockerProxyTest.java
+++ src/test/java/org/apache/mesos/mini/DockerProxyTest.java
@@ -59,24 +59,4 @@ public class DockerProxyTest {
         String url = "http://" + ipAddress + ":" + HelloWorldContainer.PORT;
         Assert.assertEquals(200, Unirest.get(url).asString().getStatus());
     }
-
-    class HelloWorldContainer extends AbstractContainer {
-
-        public static final String HELLO_WORLD_IMAGE = "tutum/hello-world";
-        public static final int PORT = 80;
-
-        protected HelloWorldContainer(DockerClient dockerClient) {
-            super(dockerClient);
-        }
-
-        @Override
-        protected void pullImage() {
-            pullImage(HELLO_WORLD_IMAGE, "latest");
-        }
-
-        @Override
-        protected CreateContainerCmd dockerCommand() {
-            return dockerClient.createContainerCmd(HELLO_WORLD_IMAGE).withName("hello-world_" + new SecureRandom().nextInt());
-        }
-    }
 }
