aabf3a2f532539711661db00fa754d343b8df125
Frank Scholten <frank@frankscholten.nl>
2015-09-01 13:50:43 +0200

    #44 Upgraded mesos-local container to 0.2.1

--- src/main/java/org/apache/mesos/mini/MesosCluster.java
+++ src/main/java/org/apache/mesos/mini/MesosCluster.java
@@ -54,10 +54,15 @@ public class MesosCluster extends ExternalResource {
         LOGGER.info("Starting Mesos cluster");
 
         try {
-            LOGGER.info("Starting Proxy");
-            DockerProxy dockerProxy = new DockerProxy(config.dockerClient, config.proxyPort);
-            addAndStartContainer(dockerProxy);
-            LOGGER.info("Started Proxy at " + dockerProxy.getIpAddress() + ":" + config.proxyPort);
+            String os = System.getProperty("os.name");
+            if (!os.equals("Linux")) {
+                LOGGER.info("Mini-Mesos runs on '" + os + "'. Starting HTTP Proxy for container networking");
+                DockerProxy dockerProxy = new DockerProxy(config.dockerClient, config.proxyPort);
+                addAndStartContainer(dockerProxy);
+                LOGGER.info("Started Proxy at " + dockerProxy.getIpAddress() + ":" + config.proxyPort);
+            } else {
+                LOGGER.info("Mini-Mesos runs on 'Linux'. No need to run HTTP Proxy for container networking");
+            }
 
             LOGGER.info("Starting Registry");
             PrivateDockerRegistry privateDockerRegistry = new PrivateDockerRegistry(config.dockerClient, this.config);
