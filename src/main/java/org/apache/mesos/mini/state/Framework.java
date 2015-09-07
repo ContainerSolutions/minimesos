b9c71c2cfc6fac7eb5feb989d5f7c6392d65a1cf
James Fisher <jhf@trifork.com>
2015-09-07 15:09:32 +0200

    add some more framework properties present in Mesos master /state.json

--- src/main/java/org/apache/mesos/mini/state/Framework.java
+++ src/main/java/org/apache/mesos/mini/state/Framework.java
@@ -10,8 +10,54 @@ import java.util.ArrayList;
 @JsonIgnoreProperties(ignoreUnknown = true)
 public class Framework {
 
+    private boolean active;
+    private boolean checkpoint;
+    private int failover_timeout;
+    private String hostname;
+    private String id;
     private String name;
+    private String role;
     private ArrayList<Task> tasks;
+
+    public boolean isActive() {
+        return active;
+    }
+
+    public void setActive(boolean active) {
+        this.active = active;
+    }
+
+    public boolean isCheckpoint() {
+        return checkpoint;
+    }
+
+    public void setCheckpoint(boolean checkpoint) {
+        this.checkpoint = checkpoint;
+    }
+
+    public int getFailoverTimeout() {
+        return failover_timeout;
+    }
+
+    public void setFailoverTimeout(int failoverTimeout) {
+        this.failover_timeout = failoverTimeout;
+    }
+
+    public String getHostname() {
+        return hostname;
+    }
+
+    public void setHostname(String hostname) {
+        this.hostname = hostname;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
 
     public String getName() {
         return name;
@@ -19,6 +65,14 @@ public class Framework {
 
     public void setName(String name) {
         this.name = name;
+    }
+
+    public String getRole() {
+        return role;
+    }
+
+    public void setRole(String role) {
+        this.role = role;
     }
 
     public ArrayList<Task> getTasks() {
