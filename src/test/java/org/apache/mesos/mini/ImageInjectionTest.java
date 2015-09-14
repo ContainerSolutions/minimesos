13fc7d97adec8c2e57a3acbc869924521c33dc31
James Fisher <jhf@trifork.com>
2015-09-14 13:27:09 +0100

    Separate MesosClusterConfig from MesosCluster

--- src/test/java/org/apache/mesos/mini/ImageInjectionTest.java
+++ src/test/java/org/apache/mesos/mini/ImageInjectionTest.java
@@ -7,11 +7,13 @@ import org.junit.Test;
 public class ImageInjectionTest {
 
     @ClassRule
-    public static final MesosCluster cluster = MesosCluster.builder()
+    public static final MesosCluster cluster = new MesosCluster(
+        MesosClusterConfig.builder()
             .numberOfSlaves(3)
             .privateRegistryPort(16000) // Currently you have to choose an available port by yourself
             .slaveResources(new String[]{"ports(*):[9200-9200,9300-9300]", "ports(*):[9201-9201,9301-9301]", "ports(*):[9202-9202,9302-9302]"})
-            .build();
+            .build()
+    );
 
     @Test
     public void testInjectImage() {
--- src/test/java/org/apache/mesos/mini/MesosClusterTest.java
+++ src/test/java/org/apache/mesos/mini/MesosClusterTest.java
@@ -13,11 +13,13 @@ import org.junit.Test;
 public class MesosClusterTest {
 
     @ClassRule
-    public static final MesosCluster cluster = MesosClusterConfig.builder()
+    public static final MesosCluster cluster = new MesosCluster(
+        MesosClusterConfig.builder()
             .numberOfSlaves(3)
             .privateRegistryPort(16000) // Currently you have to choose an available port by yourself
             .slaveResources(new String[]{"ports(*):[9200-9200,9300-9300]", "ports(*):[9201-9201,9301-9301]", "ports(*):[9202-9202,9302-9302]"})
-            .build();
+            .build()
+    );
 
     @Test
     public void mesosClusterStateInfoJSONMatchesSchema() throws UnirestException, JsonParseException, JsonMappingException {
