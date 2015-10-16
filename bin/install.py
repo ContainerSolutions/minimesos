ebefbc76323bef665b59817aa5959a76012a0515
Frank Scholten <frank@frankscholten.nl>
2015-10-16 12:39:56 +0200

    #105 Added initial script which checks the system.

---
+++ bin/install.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python
+#
+# This script is meant for quick & easy install via:
+#   'sudo curl -sSL https://raw.githubusercontent.com/ContainerSolutions/mini-mesos/master/bin/install | bash'
+
+import platform
+
+print "This script will install MiniMesos 0.5.0. Continue? [Y/N]"
+
+# TODO: Continue?
+
+print "[Detected system] - " + platform.system()
