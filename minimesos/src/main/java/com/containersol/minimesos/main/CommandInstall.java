002d0b3860c4faf26bc1cde8a5b61309385d6fbc
Frank Scholten <frank@frankscholten.nl>
2015-10-22 16:34:57 +0200

    #93Added cli command
     TODO: Does not yet deploy properly with 3 slaves.

--- minimesos/src/main/java/com/containersol/minimesos/main/CommandInstall.java
+++ minimesos/src/main/java/com/containersol/minimesos/main/CommandInstall.java
@@ -3,17 +3,24 @@ package com.containersol.minimesos.main;
 import com.beust.jcommander.Parameter;
 import com.beust.jcommander.Parameters;
 
+import java.util.List;
+
 /**
  * Installs a framework with Marathon
  */
-@Parameters(separators = "=", commandDescription = "Install a framework with Marathon")
+@Parameters(commandDescription = "Install a framework with Marathon")
 public class CommandInstall {
 
-    @Parameter(description = "The Marathon JSON file.")
-    String marathonFile;
+    @Parameter
+    List<String> marathonFiles;
 
     public String getMarathonFile() {
-        return marathonFile;
+        String marathonFile = marathonFiles.get(0);
+        if (marathonFile != null) {
+            return marathonFile;
+        } else {
+            return null;
+        }
     }
 
 }
