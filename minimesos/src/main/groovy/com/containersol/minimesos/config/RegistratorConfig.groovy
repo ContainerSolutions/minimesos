78d192089051e71dfbc555cd325f9249f7bff936
Remmelt Pit <remmelt.pit@container-solutions.com>
2016-03-11 12:05:22 +0100

    Specific version number for the Registrator image instead of latest

--- minimesos/src/main/groovy/com/containersol/minimesos/config/RegistratorConfig.groovy
+++ minimesos/src/main/groovy/com/containersol/minimesos/config/RegistratorConfig.groovy
@@ -3,7 +3,7 @@ package com.containersol.minimesos.config;
 public class RegistratorConfig extends GroovyBlock implements ContainerConfig {
 
     public static final String REGISTRATOR_IMAGE_NAME = "gliderlabs/registrator";
-    public static final String REGISTRATOR_TAG_NAME = "latest";
+    public static final String REGISTRATOR_TAG_NAME = "v6";
 
     String imageName     = REGISTRATOR_IMAGE_NAME
     String imageTag      = REGISTRATOR_TAG_NAME
