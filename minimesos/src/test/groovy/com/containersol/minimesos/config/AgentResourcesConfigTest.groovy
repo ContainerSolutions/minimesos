320f2c06411a5d5bdb0792141484c4042fb5a96c
Viktor Sadovnikov <viktor@jv-ration.com>
2016-03-14 13:08:54 +0100

    fixes #296. Parsing resources with multiple ranges of ports

--- minimesos/src/test/groovy/com/containersol/minimesos/config/AgentResourcesConfigTest.groovy
+++ minimesos/src/test/groovy/com/containersol/minimesos/config/AgentResourcesConfigTest.groovy
@@ -50,4 +50,22 @@ public class AgentResourcesConfigTest {
 
     }
 
+    @Test
+    public void testElasticSearchResources() {
+
+        String resources = "ports(testRole):[9200-9200,9300-9300]; cpus(testRole):0.2; mem(testRole):256; disk(testRole):200"
+        AgentResourcesConfig resourcesConfig = AgentResourcesConfig.fromString(resources)
+
+        assertEquals("one role is expected for ports", 1, resourcesConfig.ports.size())
+        assertEquals("one role is expected for cpus", 1, resourcesConfig.cpus.size())
+        assertEquals("one role is expected for mem", 1, resourcesConfig.mems.size())
+        assertEquals("one role is expected for disk", 1, resourcesConfig.disks.size())
+
+        assertEquals( "[9200-9200,9300-9300]", resourcesConfig.ports["testRole"].value)
+        assertEquals( 0.2, resourcesConfig.cpus["testRole"].value, 0.0001)
+        assertEquals( 256, resourcesConfig.mems["testRole"].value, 0.0001)
+        assertEquals( 200, resourcesConfig.disks["testRole"].value, 0.0001)
+
+    }
+
 }
