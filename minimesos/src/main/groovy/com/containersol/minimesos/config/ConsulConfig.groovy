df12f4f15fc573e6185abbd7cfcff8895fbdafa4
Viktor Sadovnikov <viktor@jv-ration.com>
2016-03-15 11:59:57 +0100

    cosmetic groovy code changes

--- minimesos/src/main/groovy/com/containersol/minimesos/config/ConsulConfig.groovy
+++ minimesos/src/main/groovy/com/containersol/minimesos/config/ConsulConfig.groovy
@@ -2,8 +2,8 @@ package com.containersol.minimesos.config;
 
 public class ConsulConfig extends GroovyBlock implements ContainerConfig {
 
-    public static final String CONSUL_IMAGE_NAME = "containersol/consul-server";
-    public static final String CONSUL_TAG_NAME = "0.6";
+    public static final String CONSUL_IMAGE_NAME = "containersol/consul-server"
+    public static final String CONSUL_TAG_NAME = "0.6"
 
     public static final int CONSUL_HTTP_PORT = 8500
     public static final int CONSUL_DNS_PORT = 8600
