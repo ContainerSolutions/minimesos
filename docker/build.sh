015e2553d222518c4158705809d475e5490f1f6d
Aleksandr Guljajev <gulyayev.alex@gmail.com>
2016-03-16 12:14:00 +0100

    Updating build.sh

--- docker/build.sh
+++ docker/build.sh
@@ -1,36 +1,24 @@
 #!/bin/bash
 
+
+buildVersion() {
+    cd $1
+
+    cd agent
+    docker build -t containersol/mesos-agent:$1 .
+    cd ..
+
+    cd master
+    docker build -t containersol/mesos-master:$1 .
+    cd ..
+    cd ..
+}
+
 cd base
 docker build -t containersol/mesos-base:latest .
 cd ..
 
-cd 0.25.0-0.2.70.ubuntu1404
-
-cd agent
-docker build -t containersol/mesos-agent:0.25.0-0.2.70.ubuntu1404 .
-cd ..
-
-cd master
-docker build -t containersol/mesos-master:0.25.0-0.2.70.ubuntu1404 .
-cd ..
-cd ..
-
-cd 0.26.0-0.2.145.ubuntu1404
-
-cd agent
-docker build -t containersol/mesos-agent:0.26.0-0.2.145.ubuntu1404 .
-cd ..
-
-cd master
-docker build -t containersol/minimesos-master:0.26.0-0.2.145.ubuntu1404 .
-cd ..
-cd ..
-
-cd 0.27.0-0.2.190.ubuntu1404
-
-cd agent
-docker build -t containersol/mesos-agent:0.27.0-0.2.190.ubuntu1404 .
-cd ..
-
-cd master
-docker build -t containersol/minimesos-master:0.27.0-0.2.190.ubuntu1404 .
+buildVersion "0.25.0-0.2.70.ubuntu1404"
+buildVersion "0.26.0-0.2.145.ubuntu1404"
+buildVersion "0.27.0-0.2.190.ubuntu1404"
+buildVersion "0.27.1-2.0.226.ubuntu1404"
