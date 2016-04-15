5be6180a23a07980d2ee37ea1d153a43b22663db
Viktor Sadovnikov <viktor@jv-ration.com>
2016-04-15 11:55:05 +0200

    #386. Connecting SonarQube and GitHub

--- travis.sh
+++ travis.sh
@@ -3,7 +3,7 @@ set -ev
 
 GH_SONARQ_PARAMS=""
 
-if [ [ ! -z "$TRAVIS_PULL_REQUEST" ] && "${TRAVIS_PULL_REQUEST}" != "false" ]; then
+if [ ! -z "$TRAVIS_PULL_REQUEST" ] && [ "${TRAVIS_PULL_REQUEST}" != "false" ]; then
     echo "PR build. Will execute SonarQube preview scan"
     GH_SONARQ_PARAMS="jacocoTestReport sonarqube -Dsonar.analysis.mode=preview -Dsonar.host.url=$SQ_URL -Dsonar.github.oauth=$GH_TOKEN -Dsonar.github.repository=$TRAVIS_REPO_SLUG -Dsonar.github.pullRequest=$TRAVIS_PULL_REQUEST"
 fi
