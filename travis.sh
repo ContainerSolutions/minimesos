4b6a46dbc6fb7200d2f69352c67faa793235eb17
Viktor Sadovnikov <viktor@jv-ration.com>
2016-04-18 16:15:21 +0200

    running regular SonarQube scan on this branch (later on master)

--- travis.sh
+++ travis.sh
@@ -10,4 +10,10 @@ if [ ! -z "$TRAVIS_PULL_REQUEST" ] && [ "${TRAVIS_PULL_REQUEST}" != "false" ]; t
     GH_SONARQ_PARAMS="jacocoTestReport sonarqube -Dsonar.analysis.mode=preview -Dsonar.host.url=$SQ_URL -Dsonar.github.oauth=$GH_TOKEN -Dsonar.github.repository=$TRAVIS_REPO_SLUG -Dsonar.github.pullRequest=$TRAVIS_PULL_REQUEST"
 fi
 
+if [ "${TRAVIS_PULL_REQUEST}" == "false" ] && [ "${TRAVIS_BRANCH}" == "build/removal-of-jenkins" ]; then
+    echo "Building $TRAVIS_BRANCH branch. Will execute SonarQube scan"
+    GH_SONARQ_PARAMS="jacocoTestReport sonarqube -Dsonar.host.url=$SQ_URL -Dsonar.jdbc.url=$SQ_JDBC_URL -Dsonar.jdbc.driverClassName=org.postgresql.Driver -Dsonar.jdbc.user=$SQ_JDBC_USER -Dsonar.jdbc.password=$SQ_JDBC_PASSWORD"
+fi
+
 ./gradlew --info --stacktrace clean build $GH_SONARQ_PARAMS
+
