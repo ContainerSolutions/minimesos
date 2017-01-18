09f257dc2d1ba87d84c4fc6fa4b6da1d84c10ac1
Frank Scholten <frank@frankscholten.nl>
2017-01-18 11:20:41 +0100

    Increased default memory to 1024 MB.

--- minimesos/src/main/groovy/com/containersol/minimesos/config/AgentResourcesConfig.groovy
+++ minimesos/src/main/groovy/com/containersol/minimesos/config/AgentResourcesConfig.groovy
@@ -8,7 +8,7 @@ import java.util.regex.Matcher
 class AgentResourcesConfig extends GroovyBlock {
 
     static public final ResourceDefScalar DEFAULT_CPU = new ResourceDefScalar("*", 4)
-    static public final ResourceDefScalar DEFAULT_MEM = new ResourceDefScalar("*", 512)
+    static public final ResourceDefScalar DEFAULT_MEM = new ResourceDefScalar("*", 1024)
     static public final ResourceDefScalar DEFAULT_DISK = new ResourceDefScalar("*", 2000)
     static public final ResourceDefRanges DEFAULT_PORTS = new ResourceDefRanges("*", "[31000-32000]")
 
